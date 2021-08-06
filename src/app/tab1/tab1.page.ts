2269093dc7ab33f3bdb5f8be76991333a324d572
Matias Ghiglione <mghiglione@ombutech.net>
2021-08-05 22:32:49 -0300

    scanner app complete

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  swiperOpts = {
+    allowSlidePrev: false,
+    allowSlideNext: false,
+  };
+
+  constructor(private barcodeScanner: BarcodeScanner) {}
+
+  ionViewWillEnter(){
+    console.log('Prueba');
+    this.scan();
+  }
+
+  scan() {
+    this.barcodeScanner.scan().then(barcodeData => {
+        console.log('Barcode data', barcodeData);
+      }).catch(err => {
+        console.log('Error', err);
+      });
+  }
 
 }
+
+
